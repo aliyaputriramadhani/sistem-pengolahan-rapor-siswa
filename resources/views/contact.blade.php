b18f08bc0319dd5fb8cc57b4c8b97feee50e6ff4
aliyaputriramadhani <aliya.putriramadhani01@gmail.com>
2026-03-07 21:22:48 +0700

    Update Readme, dan penambahan file baru tes02.php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <p>Silahkan menghubungi kontak kami yang tertera dibawah ini 
+    ....... </p>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+
+Selamat Datang di Toko Baju Sewa Terbaik {{$nama}} <br>
+Anda adalah seorang {{$pekerjaan}}
+</div>
